de145fca49c88d8a9dd74a0a1d4a8592400d9c1b
ultra-white <obukhov.dmitry143@gmail.com>
2025-10-05 07:24:14 +0300

    fix: strapi new version reqss

--- backend/config/server.ts
+++ backend/config/server.ts
@@ -1,7 +1,9 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
+	host: env("HOST", "0.0.0.0"),
+	port: env.int("PORT", 1337),
+	app: {
+		keys: env.array("APP_KEYS"),
+	},
+	url: env("SERVER_URL", "http://0.0.0.0:1337"),
+	proxy: true,
 });
